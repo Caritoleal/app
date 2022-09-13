6bc77d5eca566f500a93a2186a68d3fa732ce526
Caritoleal <109706077+Caritoleal@users.noreply.github.com>
2022-09-13 00:35:06 -0500

    Actualización 13/09/2022

--- src/main/java/com/creativeminds/app/AppApplication.java
+++ src/main/java/com/creativeminds/app/AppApplication.java
@@ -4,10 +4,11 @@ import com.creativeminds.app.model.Empleado;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 @SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class AppApplication {
 
